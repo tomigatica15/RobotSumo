3c9ffa87b3d54f1172dbfc24b1bfafa3085095c1
tomigatica15 <gaticatomi@gmail.com>
2023-08-23 18:45:07 -0300

    robot

--- Robot.ino
+++ Robot.ino
@@ -1,4 +1,4 @@
-/* 
+/*
 Tomas Gatica
 Robot sumo
 5 E
@@ -11,17 +11,17 @@ const int trig = 2;
 const int echo = 3;
 
 const int sensor_front_right = A0;
-const int sensor_back_right = 5;
-const int sensor_front_left = 6;
-const int sensor_back_left = 7;
+const int sensor_back_right = A1;
+const int sensor_front_left = A2;
+const int sensor_back_left = A3;
 
 #define motor_right 8
 #define motor_right2 9
 #define motor_left 10
 #define motor_left2 11
 
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   // Ultrasonic
   pinMode(trig, OUTPUT);
@@ -42,25 +42,29 @@ void setup() {
 //  Movements //
 //            //
 
-void leftBack() {
+void leftBack()
+{
   digitalWrite(motor_left, LOW);
   digitalWrite(motor_left2, HIGH);
   delay(1);
 }
 
-void leftFront() {
+void leftFront()
+{
   digitalWrite(motor_left, HIGH);
   digitalWrite(motor_left2, LOW);
   delay(1);
 }
 
-void rightBack() {
+void rightBack()
+{
   digitalWrite(motor_right, LOW);
   digitalWrite(motor_right2, HIGH);
   delay(10);
 }
 
-void rightFront() {
+void rightFront()
+{
   digitalWrite(motor_right, HIGH);
   digitalWrite(motor_right2, LOW);
   delay(10);
@@ -70,56 +74,107 @@ void rightFront() {
 //  Optic sensor  //
 //                //
 
-void sensor() {
+void sensor()
+{
   int value_front_left = analogRead(sensor_front_left);
   int value_back_left = analogRead(sensor_back_left);
 
   int value_front_right = analogRead(sensor_front_right);
   int value_back_right = analogRead(sensor_back_right);
-  
-  
-  /* 0000 */        if (value_front_left > 500 && value_front_right > 500 && value_back_left > 500 && value_back_right > 500) {  //
 
-  /* 0001 */ } else if (value_front_left > 500 && value_front_right > 500 && value_back_left > 500 && value_back_right < 500) {  //
+  /* 0000 */ /* All Black */
+  if (value_front_left > 500 && value_front_right > 500 && value_back_left > 500 && value_back_right > 500)
+  {
+    rightFront();
+    leftFront();
+  }
+  /* 0001 */ /*  Right Back White */
+  else if (value_front_left > 500 && value_front_right > 500 && value_back_left > 500 && value_back_right < 500)
+  {
 
-  /* 0010 */ } else if (value_front_left > 500 && value_front_right > 500 && value_back_left < 500 && value_back_right > 500) {  // 
+  }
+  /* 0010 */ /* Left Back White */
+  else if (value_front_left > 500 && value_front_right > 500 && value_back_left < 500 && value_back_right > 500)
+  {
 
-  /* 0011 */ } else if (value_front_left > 500 && value_front_right > 500 && value_back_left < 500 && value_back_right < 500) {  //
+  }
+  /* 0011 */ /* Left & Right Back White */
+  else if (value_front_left > 500 && value_front_right > 500 && value_back_left < 500 && value_back_right < 500)
+  {
 
-  /* 0100 */ } else if (value_front_left > 500 && value_front_right < 500 && value_back_left > 500 && value_back_right > 500) {  //
-  
-  /* 0101 */ } else if (value_front_left > 500 && value_front_right < 500 && value_back_left > 500 && value_back_right < 500) {  //
-  
-  /* 0110 */ } else if (value_front_left > 500 && value_front_right < 500 && value_back_left < 500 && value_back_right > 500) {  //
-  
-  /* 0111 */ } else if (value_front_left > 500 && value_front_right < 500 && value_back_left < 500 && value_back_right < 500) {  //
-  
-  /* 1000 */ } else if (value_front_left < 500 && value_front_right > 500 && value_back_left > 500 && value_back_right > 500) {  //
-  
-  /* 1001 */ } else if (value_front_left < 500 && value_front_right > 500 && value_back_left > 500 && value_back_right < 500) {  //
-  
-  /* 1010 */ } else if (value_front_left < 500 && value_front_right > 500 && value_back_left < 500 && value_back_right > 500) {  //
-  
-  /* 1011 */ } else if (value_front_left < 500 && value_front_right > 500 && value_back_left < 500 && value_back_right < 500) {  //
-  
-  /* 1100 */ } else if (value_front_left < 500 && value_front_right > 500 && value_back_left > 500 && value_back_right > 500) {  // 
-  
-  /* 1101 */ } else if (value_front_left < 500 && value_front_right > 500 && value_back_left > 500 && value_back_right < 500) {  // 
-  
-  /* 1110 */ } else if (value_front_left < 500 && value_front_right > 500 && value_back_left < 500 && value_back_right > 500) {  // 
- 
-  /* 1111 */ } else if (value_front_left < 500 && value_front_right > 500 && value_back_left < 500 && value_back_right < 500) {  // 
+  }
+  /* 0100 */ /*  Front Right White */
+  else if (value_front_left > 500 && value_front_right < 500 && value_back_left > 500 && value_back_right > 500)
+  {
 
-             } 
+  }
+  /* 0101 */ /* Front & Back Right White */
+  else if (value_front_left > 500 && value_front_right < 500 && value_back_left > 500 && value_back_right < 500)
+  {
+
+  }
+  /* 0110 */ /* Front Right & Back Left White */
+  else if (value_front_left > 500 && value_front_right < 500 && value_back_left < 500 && value_back_right > 500)
+  {
+
+  }
+  /* 0111 */ /* Front Right & Back Left & Back Right White */
+  else if (value_front_left > 500 && value_front_right < 500 && value_back_left < 500 && value_back_right < 500)
+  {
+
+  }
+  /* 1000 */ /* Front Left White */
+  else if (value_front_left < 500 && value_front_right > 500 && value_back_left > 500 && value_back_right > 500)
+  {
+
+  }
+  /* 1001 */ /* Front Left & Back Right White */
+  else if (value_front_left < 500 && value_front_right > 500 && value_back_left > 500 && value_back_right < 500)
+  {
+
+  }
+  /* 1010 */ /*Front Left & Back Left White*/
+  else if (value_front_left < 500 && value_front_right > 500 && value_back_left < 500 && value_back_right > 500)
+  {
+
+  }
+  /* 1011 */ /* Front Left & Back Left & Back Right White */
+  else if (value_front_left < 500 && value_front_right > 500 && value_back_left < 500 && value_back_right < 500)
+  {
+
+  }
+  /* 1100 */ /* Front Left & Front Right White */
+  else if (value_front_left < 500 && value_front_right < 500 && value_back_left > 500 && value_back_right > 500)
+  {
+
+  }
+  /* 1101 */ /* Front Left & Front Right & Back Right White */
+  else if (value_front_left < 500 && value_front_right < 500 && value_back_left > 500 && value_back_right < 500)
+  {
+
+  }
+  /* 1110 */ /*Front Left & Front Right & Back Left White*/
+  else if (value_front_left < 500 && value_front_right < 500 && value_back_left < 500 && value_back_right > 500)
+  {
+    
+  }
+  /* 1111 */ /* All White */
+  else if (value_front_left < 500 && value_front_right < 500 && value_back_left < 500 && value_back_right < 500)
+  {
+    
+  }
 }
 
-
-void sensorTest() {
+void sensorTest()
+{
   float value_front_right = analogRead(sensor_front_right);
-  if(value_front_right < 500) {
+  if (value_front_right < 500)
+  {
     Serial.println("< 500");
     delay(2000);
-  } else if (value_front_right > 500) {
+  }
+  else if (value_front_right > 500)
+  {
     Serial.println("> 500");
     delay(2000);
   }
@@ -128,7 +183,8 @@ void sensorTest() {
 // Ultrasonic Sensor  //
 //                    //
 
-void ultrasonic() {
+void ultrasonic()
+{
   long distance;
   int duration;
 
@@ -141,7 +197,8 @@ void ultrasonic() {
   duration = pulseIn(echo, HIGH);
   distance = duration * 0.034 / 2;
 
-  if(distance) {
+  if (distance)
+  {
     Serial.print("Distancia: ");
     Serial.println(distance);
     delay(1000);
@@ -152,8 +209,8 @@ void ultrasonic() {
 //  Bucle //
 //        //
 
-void loop() {
+void loop()
+{
   ultrasonic();
-  //sensor();
-  //sensorTest();
+  sensor();
 }
